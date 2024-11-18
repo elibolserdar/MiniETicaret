703d99e8768d0761a907be3c975fdccac1588dec
Serdar Elibol <elibol386@gmail.com>
2024-11-18 03:04:05 +0300

    integrate bootstrap and jquery

--- ETicaretClient/src/app/admin/components/products/products.component.ts
+++ ETicaretClient/src/app/admin/components/products/products.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-products',
+  selector: 'app-admin-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
