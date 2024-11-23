6b04b6b0134be5c51e32662de195c19999d673ea
Serdar Elibol <elibol386@gmail.com>
2024-11-24 01:58:06 +0300

    customized notifications with alertify

--- ETicaretClient/src/app/admin/components/dashboard/dashboard.component.ts
+++ ETicaretClient/src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertifyService, AlertPosition, MessageType } from 'src/app/services/admin/alertify.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
 
+  constructor(private alertify: AlertifyService) {}
+
+  ngOnInit(): void {
+  }
+
+  m(){
+    this.alertify.message("Merhaba", {
+      messageType: MessageType.Success, 
+      position: AlertPosition.TopRight, 
+      delay: 10});
+  }
+
+  d(){
+    this.alertify.dismissAll();
+  }
 }
