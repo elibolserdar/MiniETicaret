9bfe415fb6142af110f22fdb0ae39d3a8b61ba4d
Serdar Elibol <elibol386@gmail.com>
2025-02-09 21:51:47 +0300

    delete confirmation with delete modal dialog

--- ETicaretClient/src/app/admin/components/products/products.module.ts
+++ ETicaretClient/src/app/admin/components/products/products.module.ts
@@ -7,10 +7,12 @@ import { CreateComponent } from './create/create.component';
 import { ListComponent } from './list/list.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {MatTableModule} from '@angular/material/table';
-import {MatPaginatorModule} from '@angular/material/paginator';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { DeleteDirective } from 'src/app/directives/admin/delete.directive';
+import { MatDialogModule } from '@angular/material/dialog';
+import { DeleteDialogComponent } from 'src/app/dialogs/delete-dialog/delete-dialog.component';
 
 
 @NgModule({
@@ -18,7 +20,8 @@ import { DeleteDirective } from 'src/app/directives/admin/delete.directive';
     ProductsComponent,
     CreateComponent,
     ListComponent,
-    DeleteDirective
+    DeleteDirective,
+    DeleteDialogComponent
   ],
   imports: [
     CommonModule,
@@ -30,7 +33,8 @@ import { DeleteDirective } from 'src/app/directives/admin/delete.directive';
     MatInputModule,
     MatButtonModule,
     MatTableModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatDialogModule,
   ]
 })
 export class ProductsModule { }
--- ETicaretClient/src/app/directives/admin/delete.directive.ts
+++ ETicaretClient/src/app/directives/admin/delete.directive.ts
@@ -1,6 +1,8 @@
-import { Directive, ElementRef, EventEmitter, HostListener, Input, Output, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, EventEmitter, HostListener, inject, Input, Output, Renderer2 } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { SpinnerType } from 'src/app/base/base.component';
+import { DeleteDialogComponent, DeleteState } from 'src/app/dialogs/delete-dialog/delete-dialog.component';
 import { AlertifyService, AlertifyMessageType as AlertMessageType, AlertPosition } from 'src/app/services/admin/alertify.service';
 import { ProductService } from 'src/app/services/common/models/product.service';
 
@@ -27,30 +29,48 @@ export class DeleteDirective {
     _renderer.appendChild(element.nativeElement, img);
   }
 
+  readonly dialog = inject(MatDialog);
+  
   @Input() id: string;
   @Output() callback: EventEmitter<any>= new EventEmitter();
   @HostListener("click")
 
   async onclick() {
-    debugger;
-    this.spinner.show(SpinnerType.BallAtom)
-    const td: HTMLTableCellElement = this.element.nativeElement;
-
-    await this.productService.delete(this.id, () => {
-      this.alertify.message("Kayıt başarıyla silinmiştir.", {
-        dismissOthers: true,
-        messageType: AlertMessageType.Success,
-        position: AlertPosition.TopRight
-      })
-      $(td.parentElement).fadeOut(1000, () => {
-        this.callback.emit();
+    this.openDialog(async() => {
+      this.spinner.show(SpinnerType.BallAtom)
+      const td: HTMLTableCellElement = this.element.nativeElement;
+  
+      await this.productService.delete(this.id, () => {
+        this.alertify.message("Kayıt başarıyla silinmiştir.", {
+          dismissOthers: true,
+          messageType: AlertMessageType.Success,
+          position: AlertPosition.TopRight
+        })
+        $(td.parentElement).animate({
+          opacity: 0,
+          left: "+=50",
+          height: "toggle"
+        }, 700, () => {
+          this.callback.emit();
+        });
+      }, errorMessage => {
+        this.alertify.message(errorMessage, {
+          dismissOthers: true,
+          messageType: AlertMessageType.Error,
+          position: AlertPosition.TopRight
+        })
       });
-    }, errorMessage => {
-      this.alertify.message(errorMessage, {
-        dismissOthers: true,
-        messageType: AlertMessageType.Error,
-        position: AlertPosition.TopRight
-      })
+    });
+  }
+
+  openDialog(afterClosed: any): void {
+    const dialogRef = this.dialog.open(DeleteDialogComponent, {
+      data: DeleteState.Yes,
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if(result == DeleteState.Yes)
+        afterClosed();
     });
   }
 }
